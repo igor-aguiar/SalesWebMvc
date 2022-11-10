73c05363d3a288877b2b4f703799dce2bc5c4cb2
igor-aguiar <desenvigor@gmail.com>
2022-11-10 20:47:19 -0300

    Add validationto Sellers properties

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -24,15 +24,19 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} can't be empty")]
+        [StringLength(50, ErrorMessage = "{0} Must be between {2} and {1} characters", MinimumLength = 3)]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [DisplayName("Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} must be filled")]
         public DateTime BirthDate { get; set; }
         [DisplayName("Base Salary")]
         [DataType(DataType.Currency)]
+        [Range(100, 3000, ErrorMessage = "{0} Must be between {1} and {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
